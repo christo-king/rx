eca9def292651e2bebe117058a6b358dd6f6dd67
Christopher King <cking@enmobilellc.com>
2017-02-01 13:33:10 -0500

    working list and now finishing the submit

--- src/jsx/app.jsx
+++ src/jsx/app.jsx
@@ -1,6 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {Button, ButtonToolbar, Jumbotron, FormGroup, ControlLabel, FormControl, HelpBlock} from 'react-bootstrap'
+import {
+    Button,
+    ButtonToolbar,
+    Jumbotron,
+    FormGroup,
+    ControlLabel,
+    FormControl,
+    HelpBlock,
+    Panel,
+    Grid, Row, Col
+} from 'react-bootstrap'
 
 export class AppHeader extends React.Component {
     render() {
@@ -13,7 +23,13 @@ export class AppHeader extends React.Component {
 }
 
 export class ListStdDevs extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {standardDeviations: []}
+    }
+
     componentDidMount() {
+        let self = this;
         fetch('standardDeviation').then((standardDeviations) => standardDeviations.json()).then((standardDeviations) => this.setState({
             standardDeviations: standardDeviations
         }));
@@ -21,7 +37,26 @@ export class ListStdDevs extends React.Component {
 
     render() {
         return (
-            <div>Current List : {this.state}</div>
+            <div>
+                <Panel collapsible defaultExpanded header="Existing Standard Deviations List">
+                    <Grid>
+                        <Row className="show-grid">
+                            <Col xs={3} md={3}><h3>ID</h3></Col>
+                            <Col xs={3} md={3}><h3>Standard Deviation</h3></Col>
+                            <Col xs={6} md={6}><h3>Data Point Input</h3></Col>
+                        </Row>
+                        {this.state.standardDeviations.map((sd) => {
+                            return (
+                                <Row className="show-grid">
+                                    <Col xs={3} md={3}>{sd.id}</Col>
+                                    <Col xs={3} md={3}>{sd.answer}</Col>
+                                    <Col xs={6} md={6}>{sd.points}</Col>
+                                </Row>
+                            )
+                        })}
+                    </Grid>
+                </Panel>
+            </div>
         )
     }
 
@@ -29,18 +64,12 @@ export class ListStdDevs extends React.Component {
 export class NewStdDevForm extends React.Component {
     constructor(props) {
         super(props)
-        this.state = {value: ''}
-    }
-
-    getValidationState() {
-        const length = this.state.value.length;
-        if (length > 10) return 'success';
-        else if (length > 5) return 'warning';
-        else if (length > 0) return 'error';
+        this.state = {value: '', validationState: ''}
     }
 
     handleChange(e) {
         this.setState({value: e.target.value});
+        console.log("Setting value to " + this.state.value)
     }
 
     render() {
@@ -48,17 +77,15 @@ export class NewStdDevForm extends React.Component {
             <form>
                 <FormGroup
                     controlId="formBasicText"
-                    validationState={this.getValidationState()}
                 >
                     <ControlLabel>Enter a list of numbers</ControlLabel>
                     <FormControl
                         type="text"
                         value={this.state.value}
                         placeholder="Example: 28 28 187 38 192 37"
-                        onChange={this.handleChange}
+                        onChange={(e) => this.handleChange(e)}
                     />
                     <FormControl.Feedback />
-                    <HelpBlock>Validation is based on string length.</HelpBlock>
                 </FormGroup>
                 <ButtonToolbar>
                     <Button>Add Standard Deviation</Button>
